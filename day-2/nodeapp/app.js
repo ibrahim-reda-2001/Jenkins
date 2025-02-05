b3dbf6a6746a86f0389bfa71631079b95680bf1f
ibrahim-reda-2001 <ibrahimworkacount@gmail.com>
2025-02-05 15:32:04 +0200

    edit

--- day-2/nodeapp/app.js
+++ day-2/nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hellllo NTI ....... ')
+    res.send('hello ya brother from another mother  ')
   })
   
   app.listen(port, () => {
