576e601767e178e133e4356ee72cb43d157f6f77
ibrahim_reda <128513557+ibrahim-reda-2001@users.noreply.github.com>
2025-02-08 19:51:15 +0200

    Update app.js

--- day-2/nodeapp/app.js
+++ day-2/nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('hello ya brother from another mother  ')
+    res.send('hello ya bro  ')
   })
   
   app.listen(port, () => {
